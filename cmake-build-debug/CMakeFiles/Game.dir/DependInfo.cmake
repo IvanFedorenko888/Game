0a57b6fb17953b14125d677fccc6b23cbba4cb63
Ivan Fedorenko <30856286+IvanFedorenko888@users.noreply.github.com>
2017-08-09 14:10:05 +0700

    add

---
+++ cmake-build-debug/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Ivan Fedorenko/CLionProjects/Game/controller.cpp" "C:/Users/Ivan Fedorenko/CLionProjects/Game/cmake-build-debug/CMakeFiles/Game.dir/controller.cpp.obj"
+  "C:/Users/Ivan Fedorenko/CLionProjects/Game/main.cpp" "C:/Users/Ivan Fedorenko/CLionProjects/Game/cmake-build-debug/CMakeFiles/Game.dir/main.cpp.obj"
+  "C:/Users/Ivan Fedorenko/CLionProjects/Game/model.cpp" "C:/Users/Ivan Fedorenko/CLionProjects/Game/cmake-build-debug/CMakeFiles/Game.dir/model.cpp.obj"
+  "C:/Users/Ivan Fedorenko/CLionProjects/Game/view.cpp" "C:/Users/Ivan Fedorenko/CLionProjects/Game/cmake-build-debug/CMakeFiles/Game.dir/view.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/main.cpp.obj"
+  "CMakeFiles/Game.dir/view.cpp.obj"
+  "CMakeFiles/Game.dir/model.cpp.obj"
+  "CMakeFiles/Game.dir/controller.cpp.obj"
+  "Game.pdb"
+  "Game.exe"
+  "Game.exe.manifest"
+  "libGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
